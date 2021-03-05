6899f475e9ca3365292e3c10343e5cccd872a00e
MorganBoy95 <55882293+MorganBoy95@users.noreply.github.com>
2021-03-05 15:29:24 +0000

    Removed login from main activity and create a login.xml Added design components for Get Started page and implemented Edit Text components & edited text types for email and passowrd

--- app/src/main/java/com/sid1629603/every_move/Login.kt
+++ app/src/main/java/com/sid1629603/every_move/Login.kt
@@ -2,14 +2,10 @@ package com.sid1629603.every_move
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
     }
-
-
 }
