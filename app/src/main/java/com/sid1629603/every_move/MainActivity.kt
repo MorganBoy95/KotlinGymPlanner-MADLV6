405fce02913e9acdc84d181cfde0bb620db1b461
MorganBoy95 <55882293+MorganBoy95@users.noreply.github.com>
2021-03-05 09:27:00 +0000

    First commit for project

---
+++ app/src/main/java/com/sid1629603/every_move/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sid1629603.every_move
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
