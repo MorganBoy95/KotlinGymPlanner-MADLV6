04e1794f619828218f6df12e91c2bd73a948082e
MorganBoy95 <55882293+MorganBoy95@users.noreply.github.com>
2021-03-05 21:49:43 +0000

    Created index page, added a Frame layout for background image and logo. Most of the stlying is complete but a few adjustments needed in areas inlcuding, fonts, padding and margins.

--- app/src/main/java/com/sid1629603/every_move/GetStarted.kt
+++ app/src/main/java/com/sid1629603/every_move/GetStarted.kt
@@ -2,10 +2,17 @@ package com.sid1629603.every_move
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class GetStarted : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.get_started)
+
+        val button = findViewById<Button>(R.id.register)
+        button.setOnClickListener{
+            val intent = Intent(this, second::class.java)
+            startActivity(intent)
+        }
     }
 }
